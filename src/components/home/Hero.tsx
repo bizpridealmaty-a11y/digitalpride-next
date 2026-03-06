62b185ca0bfc97f1294d5c763383943a82d62539
bizpridealmaty-a11y <bizpride.almaty@gmail.com>
2026-03-06 11:53:46 +0500

    Align hero cards to the right

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -178,7 +178,7 @@ export default function Hero() {
                     </motion.div>
 
                     {/* Floating navigation islands */}
-                    <div className="w-full lg:w-2/5 hidden md:flex flex-col items-center gap-5 mt-32 self-end">
+                    <div className="w-full lg:w-2/5 hidden md:flex flex-col items-end gap-5 mt-32 self-end">
                         {islands.map((island, i) => (
                             <motion.a
                                 key={i}
